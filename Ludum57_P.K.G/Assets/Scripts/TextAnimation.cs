2075887d6e792cded9359e757a4432aa16698b92
Worestesa <Worestesa@users.noreply.github.com>
2025-04-06 17:36:57 +0300

    Cod

--- Ludum57_P.K.G/Assets/Scripts/TextAnimation.cs
+++ Ludum57_P.K.G/Assets/Scripts/TextAnimation.cs
@@ -1,8 +1,10 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class TextAnimator : MonoBehaviour
 {
-    public Text textField; // Поле для вывода текста
+    public TextMeshProUGUI textField; // Поле для вывода текста
     public string fullText; // Полный текст, который нужно анимировать
     public float delayBetweenLetters = 0.1f; // Задержка между появлением букв
     private void Start()
